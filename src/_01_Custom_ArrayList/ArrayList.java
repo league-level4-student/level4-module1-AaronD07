166da5bc05584d70c92d9349803ca42f8eafa151
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-09-26 14:26:28 -0700

    Meh
    :smile:

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -14,27 +14,63 @@ public class ArrayList <T> {
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
 		
-		return ;
+		return list[loc];
 	}
 	
 	public void add(T val) {
 		T[] newList = (T[]) new Object[list.length+1];
+		for(int i=0; i<list.length; i++) {
+			newList[i]=list[i];
+		}
+		newList[newList.length-1]=val;
+		list = newList;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[]) new Object[list.length+1];
+		for(int i=0; i<list.length; i++) {
+			newList[i]=list[i];
+		}
+		newList[loc]=val;
+		list = newList;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[]) new Object[list.length+1];
+		for(int i=0; i<list.length; i++) {
+			newList[i]=list[i];
+		}
+		newList[loc]=val;
+		list = newList;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] newList = (T[]) new Object[list.length];
+		int x=0;
+		for(int i=0; i<list.length; i++) {
+			if(i==loc) {
+				x=x+1;
+			}
+			else if(x==0) {
+			newList[i]=list[i];
+		}
+			else if(x==1) {
+				newList[i-1]=list[i-1];
+			}
+			list = newList;
+			
+		}
+		
 		
 	}
 	
 	public boolean contains(T val) {
-		
-		return false;
+		T[] newList = (T[]) new Object[list.length];
+		for(int i=0; i<list.length; i++) {
+			if(newList[i] != val) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -1,9 +1,12 @@
 package _03_Switch_Statement_Practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class CustomButtonOptionPanes {
 	public static void main(String[] args) {
+		
 		String[] options = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
 		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
@@ -11,6 +14,35 @@ public class CustomButtonOptionPanes {
 
 		String choice = options[input];
 		
+		
+		switch(choice) {
+		case "Sunday": 
+			System.out.println("You have school tm. eek");
+			break;
+		case "Monday": 
+			System.out.println("Congrats it's the worst day of the week");
+			break;
+		case "Tuesday": 
+			System.out.println("Only a few more days of school");
+			break;
+		case "Wednesday": 
+			System.out.println("It's the middle of the week XD");
+			break;
+		case "Thursday": 
+			System.out.println("Wowwwww grapeeeeeee ");
+			break;
+		case "Friday": 
+			System.out.println("Yessss best day of the week, finish your homework");
+			break;
+		case "Saturday": 
+			System.out.println("Congrats if you made it this far, second best day of the week");
+			break;
+		default: 
+			System.out.println("Something went wrong :(     "
+					+ "https://www.bing.com/videos/search?q=trollz+ear+rape&docid=607997262849311141&mid=04B7A231D5FF57C4024A04B7A231D5FF57C4024A&view=detail&FORM=VIRE");
+			break;
+		}
+	}
 		//use a switch statement to do something cool for each option
 		
 	}
